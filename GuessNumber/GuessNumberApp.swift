939919a7ac48e385c5ddd5e8ef23ff6af44f6f7f
Vadim Kislov <kislov.v@gmail.com>
2022-08-18 22:04:46 +0300

    Initial Commit

---
+++ GuessNumber/GuessNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessNumberApp.swift
+//  GuessNumber
+//
+//  Created by Kislov Vadim on 18.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
